b0810ff6bf4a3808a76e35cc06345a205549ee15
RubesWorld <hi@rubesworld.live>
2021-01-11 19:50:39 -0800

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import './components/Todo.css'
+
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      tasks: tasks
+    }
+  }
+
+  handleTaskMarkThrough = (itemId) => {
+    this.setState({
+      tasks: this.state.tasks.map(tasks => {
+        if(tasks.id === itemId) {
+          return {
+            ...tasks,
+            completed: !tasks.completed
+          }
+        }
+        return(tasks);
+      })
+    });
+  }
+
+  handleTaskAdd = ( taskName ) => {
+    const task = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    }
+    const newTasks = [...this.state.tasks, task];
+
+    this.setState({
+      tasks: newTasks
+    })
+  }
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo App!</h1>
+        <TodoForm handleTaskAdd={this.handleTaskAdd}/>
+        <TodoList tasks={this.state.tasks} handleTaskMarkThrough={this.handleTaskMarkThrough}/>
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,26 @@
+import React from 'react'
+import Todo from './Todo'
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const TodoList = (props) => {
+
+    const handleClick = () => {
+        props.handleTaskCompleted();
+    }
+
+    return (
+        <div>
+            {props.tasks.map(tasks => (
+                <Todo handleTaskMarkThrough={props.handleTaskMarkThrough} key={tasks.id} tasks={tasks}/>
+            ))}
+            <button onClick={handleClick} className="clear-btn">
+                Clear Completed
+            </button>
+        </div>
+    )
+}
+
+export default TodoList;
+
